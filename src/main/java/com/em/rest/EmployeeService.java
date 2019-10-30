ed7909a205e33bbeed867992aa71b90b0a568506
Abdul Rahim Shaikh <ars23197@gmail.com>
2019-10-30 22:35:19 +0530

    Adding entity manager to clear session

--- src/main/java/com/em/rest/EmployeeService.java
+++ src/main/java/com/em/rest/EmployeeService.java
@@ -2,6 +2,8 @@ package com.em.rest;
 
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,9 +14,13 @@ public class EmployeeService {
 	@Autowired
 	private CompanyRepository cmpRepository;
 	
+	@Autowired
+	EntityManager em;
+	
 	@Transactional
 	public Optional<Company> getEmployeeDataUsingEid(String eid) {
 		Optional<Company> company = cmpRepository.findByEid(eid);
+		em.clear();
 		return cmpRepository.findByAid(company.get().getEmployees().get(0).getAid());
 	}
 }
